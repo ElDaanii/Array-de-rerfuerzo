e89083d0defd9cf0f6b43c5c8ef6c7faab569f26
Dani Arastell <daarro@floridauniversitaria.es>
2023-10-28 20:02:08 +0200

    añadiendo ej1

---
+++ arr1.php
@@ -0,0 +1,12 @@
+<?php 
+
+$meses = ["enero", "febrero", "marzo", "abril","mayo","junio","julio", "agosto", "septiempre","octubre","noviembre","diciembre",];
+
+
+$alreves = array_reverse($meses);
+
+foreach ($alreves as $mes) {
+    echo " $mes ";
+}
+
+?>
